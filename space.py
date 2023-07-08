70024ea72fede8691be6463d1f461c82dce50a89
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-08 11:28:42 -0300

    step 4

--- space.py
+++ space.py
@@ -1,35 +1,68 @@
 import time
+import asyncio
 import curses
+
+
+async def blink(canvas, row, column, symbol="*"):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
     canvas.refresh()
-    height, width = canvas.getmaxyx()
     row, column = (5, 20)
     canvas.addstr(row, column, "Hello, World!")
     canvas.border()
     canvas.refresh()
     curses.curs_set(False)
 
-    frames = [
-        "*",  # 1 кадр
-        "*",  # 2 кадр
-        "*",  # 3 кадр
-        "*",  # 4 кадр
-    ]
-    attributes = [
-        curses.A_DIM,  # Атрибут для тусклой звезды
-        curses.A_NORMAL,  # Обычная звезда
-        curses.A_BOLD,  # Атрибут для яркой звезды
-        curses.A_NORMAL,  # Обычная звезда
-    ]
+    coro = blink(canvas, 3, 3)
 
-    while True:
-        for frame, attribute in zip(frames, attributes):
-            canvas.clear()
-            canvas.addstr(5, 5, frame, attribute)
-            canvas.refresh()
-            time.sleep(1)
+    coro.send(None)
+    canvas.refresh()
+    time.sleep(1)
+    coro.send(None)
+    canvas.refresh()
+    time.sleep(1)
+    coro.send(None)
+    canvas.refresh()
+    time.sleep(1)
+    coro.send(None)
+    canvas.refresh()
+    time.sleep(1)
+    coro.send(None)
+
+    time.sleep(1)
+
+    # frames = [
+    #     "*",  # 1 кадр
+    #     "*",  # 2 кадр
+    #     "*",  # 3 кадр
+    #     "*",  # 4 кадр
+    # ]
+    # attributes = [
+    #     curses.A_DIM,  # Атрибут для тусклой звезды
+    #     curses.A_NORMAL,  # Обычная звезда
+    #     curses.A_BOLD,  # Атрибут для яркой звезды
+    #     curses.A_NORMAL,  # Обычная звезда
+    # ]
+
+    # while True:
+    #     for frame, attribute in zip(frames, attributes):
+    #         canvas.clear()
+    #         canvas.addstr(5, 5, frame, attribute)
+    #         canvas.refresh()
+    #         time.sleep(1)
 
 
 if __name__ == "__main__":
