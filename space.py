45a80f74fc95a7a84b5a1bbbf16625bbba0a8afe
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-08 14:38:36 -0300

    step 10

--- space.py
+++ space.py
@@ -29,6 +29,36 @@ async def blink(canvas, row, column, symbol="*"):
             await asyncio.sleep(0)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), "*")
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), "O")
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), " ")
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = "-" if columns_speed else "|"
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), " ")
+        row += rows_speed
+        column += columns_speed
+
+
 def draw(canvas):
     canvas.border()
     canvas.refresh()
@@ -46,6 +76,8 @@ def draw(canvas):
         )
         for _ in range(number_of_stars)
     ]
+
+    coroutines.append(fire(canvas, max_y // 2, max_x // 2))
 
     while True:
         for coroutine in coroutines.copy():
